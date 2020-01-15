2d4b6b0e1927f26620b8a072c9c4bd5225c0bfe7
lsd <1121840811@qq.com>
2020-01-15 19:30:37 +0800

    字节码文件结构剖析

---
+++ jvm/src/main/java/cn/andios/jvm/bytecode/MyTest1.java
@@ -0,0 +1,18 @@
+package cn.andios.jvm.bytecode;
+
+/**
+ * @description:
+ * @author:LSD
+ * @when:2020/01/15/14:20
+ */
+public class MyTest1 {
+    private int a = 1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
